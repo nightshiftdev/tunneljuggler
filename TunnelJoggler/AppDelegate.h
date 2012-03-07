bf986afa4a5545b1f738cc87cfa7846eb8c56ac9
pawel <pawel@pawels-macbook-pro.local>
2012-03-07 17:36:44 -0600

    Initial Commit

---
+++ TunnelJoggler/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  TunnelJoggler
+//
+//  Created by pawel on 3/7/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ TunnelJoggler/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  TunnelJoggler
+//
+//  Created by pawel on 3/7/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
